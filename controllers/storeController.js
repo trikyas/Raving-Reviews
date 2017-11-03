cc6481fe0fbda9ccf952170b792c5058f945c4b7
Trikyas <mooneychad@yahoo.com>
2017-11-04 03:02:13 +1100

    second commit
    Making it better 🤦‍♂️

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,69 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    } else {
+      next({message: 'The image type is not supported!'}, false);
+    }
+  }
+};
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 };
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Make your Raving Review!'});
 };
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.upload = multer(multerOptions).single('photo');
+exports.resize = async (req, res, next) => {
+  if(!req.file) {
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
+}
+
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Thank you for creating: ${store.name}. We\'d love you to leave a review.`)
+  res.redirect(`/store/${store.slug}`);
+};
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Raving Reviews!', stores: stores });
+};
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', { title: `Edit ${store.name}`, store: store})
+};
+exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point';
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store Now →</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+};
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if(!store) return next();
+  res.render('store', { store, title: store.name });
+};
+
+exports.getStoresByTag = async (req, res) => {
+  const tags = await Store.getTagsList();
+  const tag = req.params.tag;
+  res.render('tag', { tags, title: 'Tags', tag});
 };
--- models/Store.js
+++ models/Store.js
@@ -14,15 +14,50 @@ const storeSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  tags: [String]
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [
+      {
+        type: Number,
+        required: 'Come on. Really.'
+      }
+    ],
+    address: {
+      type: String,
+      required: 'Come on mate. Address!'
+    }
+  },
+  photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next();
     return;
   }
   this.slug = slug(this.name);
+  //find if any duplicates
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if(storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
 });
+
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 }}},
+    { $sort: { count: 1}}
+  ]);
+};
 module.exports = mongoose.model('Store', storeSchema);
